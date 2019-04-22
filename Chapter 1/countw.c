72844af9c13202513cf040ac409028c875f3c42f
Carlos Conley <carlosconley@icloud.com>
2019-04-21 22:59:32 -0500

    completed and compiled countw.c

--- Chapter 1/countw.c
+++ Chapter 1/countw.c
@@ -1,2 +1,18 @@
 /* countw.c -- counts the amount of white space in user input
 */
+
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+
+	char ch; // Character for user input
+	int white_space; // Counter
+	while ((ch = fgetc(stdin)) != EOF) {
+		if (ch == '\n' || ch == ' ' || ch == '\t')
+			++white_space;
+	}
+
+	printf("The number of white space was %d\n", white_space);
+	return 0;
+}
